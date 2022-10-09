23d133cb543835f70ee7ec074d68e9f218e925da
florei <florentina.alexa6@gmail.com>
2022-10-09 21:55:17 +0300

    public class Main {     public static void main(String[] args) {         System.out.println("Hello");         String name="Florentina";         System.out.println(name);         int suma = 100+300;         System.out.println(suma);         int x = 10;         float y = 8;         float impartire = x/y;         System.out.println(impartire);         int a = -5+ (8*6);         System.out.println(a);         float b = (55+9) % 9;         System.out.println(b);         int z = x + 10;         float w = (-3*5)/y;         float adunare = z+w;         System.out.println(adunare);

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Flori!");
+        System.out.println("Hello");
+        String name="Florentina";
+        System.out.println(name);
+        int suma = 100+300;
+        System.out.println(suma);
+        int x = 10;
+        float y = 8;
+        float impartire = x/y;
+        System.out.println(impartire);
+        int a = -5+ (8*6);
+        System.out.println(a);
+        float b = (55+9) % 9;
+        System.out.println(b);
+        int z = x + 10;
+        float w = (-3*5)/y;
+        float adunare = z+w;
+        System.out.println(adunare);
+        float Q = 5+x;
+        int S = 3*2;
+        float I = Q/S;
+        System.out.println(I);
+        int modul = 8 % 3;
+        System.out.println(modul);
+        float d = 5+ I - modul;
+        System.out.println(d);
+
     }
+
 }
